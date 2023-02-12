7f22fe7d6af1f342224758221ff5e6edc6e58ccd
slthurman01 <slthurman01@gmail.com>
2023-02-12 13:55:24 -0600

    Adds user routes to api routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addNewFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+// /api/users
+router
+    .route('/')
+    .get(getUsers)
+    .post(createUser);
+
+// /api/users/:userId
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router
+    .route('/:userId/friends/:friendId')
+    .post(addNewFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
